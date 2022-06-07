b4dc3eb7da436048bca295eb3758eeca580d71f4
pkgmain <blazer.forks0y@icloud.com>
2022-06-06 22:45:19 -0400

    Fixed router scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,12 @@
 import routes from "./routes";
+import { createMemoryHistory, createWebHistory } from "vue-router";
 
 export default {
+  history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition;
+
+    return { top: 0 };
+  },
 };
